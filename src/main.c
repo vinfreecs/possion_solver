50bb3f3ffdc9b8494d2b3a56c49af0d36b4e7d13
Vinay Kumar Reddy Patil <89305950+vinfreecs@users.noreply.github.com>
2026-01-16 09:46:02 +0100

    updated cuda kernals

--- src/main.c
+++ src/main.c
@@ -54,6 +54,8 @@ int main(int argc, char **argv) {
 
   cudaMemcpy(p_d, solver.p, size_p, cudaMemcpyHostToDevice);
   cudaMemcpy(rhs_d, solver.rhs, size_rhs, cudaMemcpyHostToDevice);
+  int threadsPerBlock = 256;
+  int blocksPerGrid = (num_devices + threadsPerBlock - 1) / threadsPerBlock;
 
   solve(&solver);
 
@@ -65,8 +67,46 @@ int main(int argc, char **argv) {
     kernal_to_do_stencil_op
     allreduce -> atomic reduce
   }
+*/
+  double r;
+  int it = 0;
+  double res, res1;
 
-   */
+  int imax = solver.imax;
+  int jmax = solver.jmax;
+  int jmaxLocal = solver.jmaxLocal;
+  double eps = solver.eps;
+  double omega = solver.omega;
+  int itermax = solver.itermax;
+
+  double dx2 = solver.dx * solver.dx;
+  double dy2 = solver.dy * solver.dy;
+  double idx2 = 1.0 / dx2;
+  double idy2 = 1.0 / dy2;
+  double factor = omega * 0.5 * (dx2 * dy2) / (dx2 + dy2);
+  double *p = solver.p;
+  double *rhs = solver.rhs;
+  double epssq = eps * eps;
+  double size = solver.size;
+
+  while ((res >= epssq) && (it < itermax)) {
+    res = 0.0;
+    exchange_cuda<blocksPerGrid, threadsPerBlock>(rank, size, p_d, jmaxLocal,
+                                                  imax);
+    stencil_cuda<blocksPerGrid, threadsPerBlock>(
+        res, eps, factor, imax, jmaxLocal, r, idx2, idy2, rhs_d, p_d);
+    outer_boundary_cuda<blocksPerGrid, threadsPerBlock>(p_d, rank, size, imax,
+                                                        jmaxLocal);
+    MPI_Allreduce(&res, &res1, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    res = res1;
+    res = sqrt(res / (imax * jmax));
+    it++;
+  }
+
+  if (rank == 0) {
+    cudaMemcpy(solver.p, p_d, size_p, cudaMemcpyDeviceToHost);
+    cudaMemcpy(solver.rhs, rhs_d, size_rhs, cudaMemcpyDeviceToHost);
+  }
 
   getResult(&solver);
 
